88b9067a3514da162865151d2033870551cc783c
manuel blanco <mandresblac@gmail.com>
2023-08-02 20:50:39 -0500

    commit 10

---
+++ projects/04-prueba-tecnica-react/src/Components/Otro.jsx
@@ -0,0 +1,11 @@
+import { useCatImage } from "../hooks/useCatImage.js";
+
+export function Otro() {
+  const { imageUrl } = useCatImage({ fact: 'cat' });
+
+  return(
+    <>
+      {imageUrl && <img src={imageUrl} alt='' />}
+    </>
+  )
+};
