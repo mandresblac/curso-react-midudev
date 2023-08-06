53a51ede571a02fe600b7f90680fbb00b5092436
manuel blanco <mandresblac@gmail.com>
2023-08-06 16:46:10 -0500

    commit 13

--- projects/05-buscador-peliculas/src/App.jsx
+++ projects/05-buscador-peliculas/src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
+import { useMovies } from './hocks/useMovies';
+import { Movies } from './components/Movies.jsx';
+import { useState, useEffect, useRef } from 'react';
+
+function useSearch() {
+  const [search, updateSearch] = useState('');
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === '';
+      return;
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una pelicula vacía');
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una pelicula con un número');
+      return;
+    }
+
+    if (search.length < 3) {
+      setError('La busqueda debe tener al menos 3 caracteres.');
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sort, setSort] = useState(false);
+
+  const { search, updateSearch, error } = useSearch();
+  const { movies, loading, getMovies } = useMovies({ search });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getMovies();
+  };
+
+  const handleSort = () => {
+    setSort(!sort);
+  };
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="page">
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent',
+            }}
+            onChange={handleChange}
+            value={search}
+            name="query"
+            type="text"
+            placeholder="Avengers, Start Wars, The Matrix..."
+          />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
+          <button type="submit">Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>{loading ? <p>Cargando...</p> : <Movies movies={movies} />}</main>
+    </div>
+  );
 }
 
 export default App
---
+++ projects/05-buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function ListOfMovies({ movies }) {
+  return (
+    <ul className="movies">
+      {movies.map((movie) => (
+        <li className='movie' key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>Año: {movie.year}</p>
+          <img src={movie.image} alt={movie.title} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function NoMoviesResults() {
+  return <p>No se encontraron peliculas para esta búsqueda.</p>;
+}
+
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0;
+
+  return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />;
+}
